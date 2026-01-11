324a71174abfde1edf45efb2474b809a6fbbe749
Andrej <pytpoint@gmail.com>
2026-01-11 22:33:28 +0100

    first

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-print("Hello git")
-print("Hello World")
-print("Hello World")
+# from datetime import datetime
+
+
+
+# def get_days_from_today(date):
+#     try:
+#         givendate = datetime.strptime(date, "%Y-%m-%d").date()
+#         today = datetime.today().date()
+#         raznica = (today - givendate).days
+#         return raznica
+#     except (TypeError, ValueError): 
+#         return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
